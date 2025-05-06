fc3cc61e6e2057d3189179bb27ef669762432f76
eng-mat <107753600+eng-mat@users.noreply.github.com>
2025-05-05 23:42:22 -0500

    Update secret-manager.sh

--- secret-manager.sh
+++ secret-manager.sh
@@ -13,3 +13,17 @@ echo -n "your-infoblox-password" | gcloud secrets create infoblox-password \
   --locations=us-central1 \
   --data-file=- \
   --kms-key=projects/my-project/locations/us-central1/keyRings/my-keyring/cryptoKeys/my-key
+
+
+
+  gcloud secrets create "username" \
+    --replication="user-managed" \
+    --locations="us-west2" \
+    --data-file="username.txt" \
+    --kms-key="projects/YOUR_PROJECT_ID/locations/us-west2/keyRings/YOUR_KEYRING/cryptoKeys/YOUR_KEY_NAME"
+
+gcloud secrets create "password" \
+    --replication="user-managed" \
+    --locations="us-west2" \
+    --data-file="password.txt" \
+    --kms-key="projects/YOUR_PROJECT_ID/locations/us-west2/keyRings/YOUR_KEYRING/cryptoKeys/YOUR_KEY_NAME"
